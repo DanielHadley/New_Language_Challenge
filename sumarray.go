d75b51d460ebe9c16472fc0b0bfef6fef5e98dea
DanielHadley <dphnrome@yahoo.com>
2014-06-01 23:00:15 -0400

    sum an array

---
+++ sumarray.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+)
+
+// Prints the sum of an array between 1 and 100
+func main() {
+	sum := 0
+	for i := 1; i < 100; i++ {
+		sum += i}
+	fmt.Println(sum)
+}
+
+
+
+
